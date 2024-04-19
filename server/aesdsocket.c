4552c0803f0d90b36cc28f678bc39013921caa94
MatheusOgata <matheus.ogata@outlook.com>
2024-04-19 16:49:38 -0300

    Multiple connections to the server is working

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,9 +13,10 @@
 #include <poll.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "queue.h"
 
 //Defines
-//#define PRINT_LOG
+#define PRINT_LOG
 #define BUFFER_SIZE 100
 
 //Macros
@@ -28,19 +29,39 @@
 //static variables
 static char ipstr[INET_ADDRSTRLEN];
 static const char* file_location = "/var/tmp/aesdsocketdata";
+static SLIST_HEAD(slisthead, slist_data_s) head;
+static bool is_signal_active = false;
+
+//structs 
+struct client_socket_data
+{
+	bool is_connection_closed;
+	int client_fd;
+	FILE* file_fd;
+	pthread_mutex_t client_mutex;
+	pthread_mutex_t* file_mutex;
+	struct sockaddr sock_addr;
+        socklen_t sock_length;
+};
+
+typedef struct slist_data_s slist_data_t;
+
+struct slist_data_s
+{
+	pthread_t thread;
+	struct client_socket_data client_info;
+	SLIST_ENTRY(slist_data_s) entries;
+};
 
 //funtons prototype
 static void signal_handler_function(int signal_number);
-static void* thread_function(void* param);
+static void* client_socket_thread_func(void* param);
+static void add_new_client(int server_fd, FILE* file, pthread_mutex_t* f_mutex);
+static void verify_disconnected_clients();
+static void remove_all_clients();
 
-//struct
-struct app_data
-{
-	int server_fd;
-        FILE* file_fd;
-	pthread_mutex_t* mutex;
-};
 
+//main function
 int main(int argc, char* argv[])
 {
 
@@ -51,10 +72,11 @@ int main(int argc, char* argv[])
 	struct addrinfo hints;
 	struct addrinfo* servinfo = NULL;
 	struct sigaction s_action;
-	struct app_data thread_data;
-	pthread_mutex_t mutex;
-	pthread_t server_thread;
+	pthread_mutex_t file_mutex;
 	FILE* file_dir = NULL;
+	struct pollfd poll_socket;
+
+	SLIST_INIT(&head);
 
         memset(&hints, 0x00, sizeof(hints));
         hints.ai_family = AF_INET;
@@ -149,7 +171,7 @@ int main(int argc, char* argv[])
                         break;
         	}
 
-        	if(pthread_mutex_init(&mutex, NULL) != 0)
+        	if(pthread_mutex_init(&file_mutex, NULL) != 0)
         	{
                 	DEBUG_LOG("Failed to initialize mutex");
                         status = -1;
@@ -172,61 +194,52 @@ int main(int argc, char* argv[])
 	}
       	while(0);
 
-	thread_data.file_fd = file_dir;
-	thread_data.server_fd = server_fd;
-	thread_data.mutex = &mutex;	
-	
-	do
+	poll_socket.fd = server_fd;
+        poll_socket.events = POLLIN;
+
+
+	for(;;)
 	{
-		if(status != 0)
-			break;
 
-		if(pthread_create(&server_thread, NULL, &thread_function, (void *) &thread_data) != 0)
-		{
-			DEBUG_LOG("It was not possible to create the thread");
-                        status = -1;
-                        break;
-		}
-
-		if(pthread_detach(server_thread) != 0)
-		{
-                        DEBUG_LOG("it was not possivel to deattach the thread\n");
-                        status = -1;
-                        break;
-		}
-
-		pause();
-
-        	if(pthread_mutex_lock(&mutex) != 0)
-        	{
-        		DEBUG_LOG("it was not possivel to lock mutex");
-                        status = -1;
-                        break;
-        	}
+		int poll_value = poll(&poll_socket, 1, 10);
 		
-		if(pthread_cancel(server_thread) != 0)
+		if(is_signal_active)
 		{
-			DEBUG_LOG("Erro to cancel the thread");
-                        status = -1;
-                        break;			
+			remove_all_clients();
+			is_signal_active = false;
+			break;
 		}
 
-        	if(pthread_mutex_unlock(&mutex) != 0)
-        	{
-                	DEBUG_LOG("it was not possivel to unlock mutex");
-                        status = -1;
-                        break;			
-        	}
+		if(poll_value < -1)
+			continue;
+
+		switch(poll_value)
+		{
+			case -1:
+		        	DEBUG_LOG("Polling has failed.\n");
+                     		continue;
+				break;
+
+			case 0:
+				verify_disconnected_clients();
+				break;
+
+			default:
+				add_new_client(server_fd, file_dir, &file_mutex);
+				break;
+
+		}
+		
 	}
-	while(0);
-	
+
+
 	DEBUG_LOG("\nClosing server..\n");
 	shutdown(server_fd, SHUT_RDWR); 
 	close(server_fd);
 	fclose(file_dir);
 
-	DEBUG_LOG("removing aesdsocketdata\n");
-	remove(file_location);	
+	//DEBUG_LOG("removing aesdsocketdata\n");
+	//remove(file_location);	
 
 	return status;
 
@@ -235,56 +248,185 @@ int main(int argc, char* argv[])
 static void signal_handler_function(int signal_number)
 {
 	syslog(LOG_DEBUG, "Caught signal, exiting");
+	is_signal_active = true;
 }
 
-static void* thread_function(void* param)
+
+
+static void add_new_client(int server_fd, FILE* file, pthread_mutex_t* f_mutex)
 {
-	struct app_data* t_data = (struct app_data*) param;
-	FILE* file_dir = t_data->file_fd;
-	int server_fd = t_data->server_fd;
-	struct sockaddr client_sock_addr;
-	struct pollfd poll_socket;
-        socklen_t client_sock_length;
-        pthread_mutex_t* mutex = t_data->mutex;
+	slist_data_t* socket = malloc(sizeof(slist_data_t));
+	struct client_socket_data* c_socket = &socket->client_info;
+
+	SLIST_INSERT_HEAD(&head, socket, entries);
+	c_socket->is_connection_closed = true;
+	c_socket->file_fd = file;
+	c_socket->file_mutex = f_mutex;
+
+        c_socket->sock_length = sizeof(struct sockaddr);
+        if((c_socket->client_fd = accept(server_fd, &c_socket->sock_addr, &c_socket->sock_length)) == -1)
+        {
+        	perror("accept error");
+                return;
+        }
+
+	      DEBUG_LOG("Client desc: %i\n", c_socket->client_fd);
+
+        memset(ipstr, 0x00, sizeof(ipstr));
+        inet_ntop(AF_INET, &(((struct sockaddr_in*) &c_socket->sock_addr)->sin_addr), ipstr, sizeof(ipstr));
+        syslog(LOG_DEBUG, "Accepted connection from %s \n", ipstr);
+        DEBUG_LOG("Accepted connection from %s \n", ipstr);
+
+
+	if(pthread_mutex_init(&c_socket->client_mutex, NULL) != 0)
+        {
+        	DEBUG_LOG("Failed to initialize mutex");
+		return;
+	}
+
+        if(pthread_create(&socket->thread, NULL, &client_socket_thread_func, (void *) c_socket) != 0)
+        {
+        	DEBUG_LOG("It was not possible to create the thread");
+		return;
+        }
+
+	c_socket->is_connection_closed = false;
+}
+
+static void verify_disconnected_clients()
+{
+    bool is_disconnected = false;
+    slist_data_t* socket = SLIST_FIRST(&head);
+    slist_data_t* next_socket = NULL;
+    struct client_socket_data* c_socket = NULL;
+
+    if(SLIST_EMPTY(&head))
+	    return;
+
+    while(1)
+    {
+	c_socket = &socket->client_info;
+
+        if(pthread_mutex_lock(&c_socket->client_mutex) != 0)
+        {
+        	DEBUG_LOG("it was not possivel to lock mutex");
+		return;
+        }
+
+	is_disconnected = c_socket->is_connection_closed;
+
+        if(pthread_mutex_unlock(&c_socket->client_mutex) != 0)
+        {
+                DEBUG_LOG("it was not possivel to lock mutex");
+                return;
+        }
+	
+	next_socket = SLIST_NEXT(socket, entries);
+
+       	if(is_disconnected)
+        {
+		DEBUG_LOG("Removing a socket...\n");
+		DEBUG_LOG("Client desc: %i\n", c_socket->client_fd);
+                if(pthread_join(socket->thread, NULL) != 0)
+                {
+                        perror("Error while joining the thread");
+                }	
+		SLIST_REMOVE(&head, socket, slist_data_s, entries);
+                free(socket);
+        }
+
+	is_disconnected = false;
+
+        if(next_socket == SLIST_END(&head))
+	{
+		break;
+	}
+	else
+	{
+		socket = next_socket;
+	}
+	      
+    }
+}
+
+static void remove_all_clients()
+{
+	slist_data_t* socket = NULL;
+	struct client_socket_data* c_socket = NULL;
+
+	while(!SLIST_EMPTY(&head))
+	{
+		socket = SLIST_FIRST(&head);
+		DEBUG_LOG("Removing clients..\n");
+	        c_socket = &socket->client_info;
+
+        	if(pthread_mutex_lock(&c_socket->client_mutex) != 0)
+        	{
+                	DEBUG_LOG("it was not possivel to lock mutex");
+                	return;
+        	}
+
+		if(pthread_cancel(socket->thread) != 0)
+                {
+                        DEBUG_LOG("Erro to cancel the thread");
+			return;
+                }
+
+        	if(pthread_mutex_unlock(&c_socket->client_mutex) != 0)
+        	{
+                	DEBUG_LOG("it was not possivel to lock mutex");
+                	return;
+        	}
+		
+                if(pthread_join(socket->thread, NULL) != 0)
+                {
+                        perror("Error while joining the thread");
+                }	
+
+		SLIST_REMOVE_HEAD(&head, entries);
+		free(socket);	
+	}
+}
+
+static void* client_socket_thread_func(void* param)
+{
+	bool is_client_disconnected = false;
+	struct client_socket_data* t_data = (struct client_socket_data*) param;
+        FILE* file_dir = t_data->file_fd;
+        int client_fd = t_data->client_fd;
+        struct pollfd poll_socket;
+        pthread_mutex_t* file_mutex = t_data->file_mutex;
+	pthread_mutex_t* client_mutex = &t_data->client_mutex;
 
         char rec_buff[BUFFER_SIZE + 1] = {'\0'};
-        int client_fd;
 
-	poll_socket.fd = server_fd;
-	poll_socket.events = POLLIN;
+        poll_socket.fd = client_fd;
+        poll_socket.events = POLLIN;
 
-        for(;;)
-        {
-		DEBUG_LOG("Polling socket conection\n");
-		if(poll(&poll_socket, 1, -1) == -1) 
-		{
-		     DEBUG_LOG("Polling has failed.\n");
-		     continue;
-		}
 
-		DEBUG_LOG("Socket conection request arrived.\n");
+	for(;;)
+	{
+		memset(rec_buff, '\0', sizeof(rec_buff));
+		DEBUG_LOG("Polling client socket for data\n");
+                if(poll(&poll_socket, 1, -1) == -1)
+                {
+                     DEBUG_LOG("Polling has failed.\n");
+                     continue;
+                }
 
-                if(pthread_mutex_lock(mutex) != 0)
+                if(pthread_mutex_lock(client_mutex) != 0)
                 {
                         DEBUG_LOG("it was not possivel to lock mutex");
-                        close(client_fd);
                         continue;
                 }
-
-		client_sock_length = sizeof(struct sockaddr);
-                if((client_fd = accept(server_fd, &client_sock_addr, &client_sock_length)) == -1)
+                
+		if(pthread_mutex_lock(file_mutex) != 0)
                 {
-                        perror("accept error");
+                      	DEBUG_LOG("it was not possivel to lock mutex");
                         continue;
                 }
 
-                memset(ipstr, 0x00, sizeof(ipstr));
-                inet_ntop(AF_INET, &(((struct sockaddr_in*) &client_sock_addr)->sin_addr), ipstr, sizeof(ipstr));
-                syslog(LOG_DEBUG, "Accepted connection from %s \n", ipstr);
-                DEBUG_LOG("Accepted connection from %s \n", ipstr);
-
-
-                do
+		do
                 {
                         int rx_size = 0;
 
@@ -298,7 +440,8 @@ static void* thread_function(void* param)
                         }
                         else if (rx_size == 0) // The remote side has closed
                         {
-                                DEBUG_LOG("the remote side has closed");
+				t_data->is_connection_closed = is_client_disconnected = true;
+                                DEBUG_LOG("the remote side has closed\n");
                                 break;
                         }
                         else if(rx_size < BUFFER_SIZE)
@@ -307,10 +450,13 @@ static void* thread_function(void* param)
                                 {
                                         DEBUG_LOG("error fwrite\n");
                                 }
-                                if(strchr(rec_buff, '\n') != NULL)
+
+                                fflush(file_dir);
+			
+				if(strchr(rec_buff, '\n') != NULL)
                                 {
                                         break;
-                                }
+                                }		
                         }
                         else
                         {
@@ -318,7 +464,10 @@ static void* thread_function(void* param)
                                 {
                                         DEBUG_LOG("error fwrite\n");
                                 }
-                                if(strchr(rec_buff, '\n') != NULL)
+
+                                fflush(file_dir);
+
+		                if(strchr(rec_buff, '\n') != NULL)
                                 {
                                         break;
                                 }
@@ -327,37 +476,18 @@ static void* thread_function(void* param)
                 }
                 while(1);
 
-                DEBUG_LOG("Sending data back..\n");
-                fseek(file_dir, 0, SEEK_SET);
-
-      
-      		do //read the content of the file and send over the socket
-                {
-
-                        memset(rec_buff, '\0', sizeof(rec_buff));
-                        if(fgets(rec_buff, BUFFER_SIZE, file_dir) == NULL)
-                        {
-                                break;
-                        }
-
-                        DEBUG_LOG("fgets result: %s", rec_buff);
-
-                        if(send(client_fd, rec_buff, strlen(rec_buff), 0) == -1)
-                        {
-                                perror("perror while sending");
-                                break;
-                        }
-                }
-                while(1);
-
-                fseek(file_dir, 0, SEEK_END);
-                close(client_fd);
-                syslog(LOG_DEBUG, "Closed connection from %s \n", ipstr);
-
-                if(pthread_mutex_unlock(mutex) != 0)
+                if(pthread_mutex_unlock(file_mutex) != 0)
                 {
                         DEBUG_LOG("it was not possivel to unlock mutex");
                 }
-        }		
+
+		if(pthread_mutex_unlock(client_mutex) != 0)
+                {
+                        DEBUG_LOG("it was not possivel to unlock mutex");
+                }
+
+		if(is_client_disconnected)
+			break;
+	}
 }
 
